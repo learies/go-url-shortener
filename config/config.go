d22ed11efb9b4f619da6bceea1a8162671ba95e8
LeAries <learies@ya.ru>
2024-09-03 22:14:40 +0300

    Add env

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"flag"
+
+	"github.com/caarlos0/env/v6"
 )
 
 const (
@@ -9,21 +11,37 @@ const (
 	DefaultBaseURL = "http://localhost:8080"
 )
 
-// Config структура, которая хранит аргументы командной строки
+// Config структура, которая хранит аргументы командной строки и переменные окружения
 type Config struct {
-	Address string
-	BaseURL string
+	Address string `env:"SERVER_ADDRESS"`
+	BaseURL string `env:"BASE_URL"`
 }
 
-// ParseConfig функция для разбора аргументов командной строки и возврата настройки
+// ParseConfig функция для разбора аргументов командной строки и переменных окружения
 func ParseConfig() *Config {
-	addressFlag := flag.String("a", DefaultAddress, "Address to start the HTTP server")
-	baseURLFlag := flag.String("b", DefaultBaseURL, "Base URL for the shortened URL")
+	cfg := &Config{
+		Address: DefaultAddress,
+		BaseURL: DefaultBaseURL,
+	}
 
+	// Парсинг переменных окружения
+	if err := env.Parse(cfg); err != nil {
+		panic(err)
+	}
+
+	// Парсинг флагов командной строки
+	addressFlag := flag.String("a", cfg.Address, "Address to start the HTTP server")
+	baseURLFlag := flag.String("b", cfg.BaseURL, "Base URL for the shortened URL")
 	flag.Parse()
 
-	return &Config{
-		Address: *addressFlag,
-		BaseURL: *baseURLFlag,
+	// Приоритизация: если флаги командной строки не заданы, оставляем значения из env
+	if addressFlag != nil && *addressFlag != DefaultAddress {
+		cfg.Address = *addressFlag
 	}
+
+	if baseURLFlag != nil && *baseURLFlag != DefaultBaseURL {
+		cfg.BaseURL = *baseURLFlag
+	}
+
+	return cfg
 }
