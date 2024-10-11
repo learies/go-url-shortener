ed5c150f0269cebd6c91ba91b2f6f75c3ad4c80e
LeAries <learies@ya.ru>
2024-10-11 09:46:27 +0300

    Add iter10

--- config/config.go
+++ config/config.go
@@ -9,6 +9,7 @@ type Config struct {
 	Address         string
 	BaseURL         string
 	FileStoragePath string
+	DatabaseDSN     string
 }
 
 func getEnv(key, defaultValue string) string {
@@ -24,21 +25,26 @@ func LoadConfig() Config {
 	defaultAddress := "localhost:8080"
 	defaultBaseURL := "http://localhost:8080"
 	var defaultFileStoragePath string
+	defaultDatabaseDSN := "postgres://postgres:postgres@localhost/postgres?sslmode=disable"
 
 	// Read from environment variables
 	envAddress := getEnv("SERVER_ADDRESS", defaultAddress)
 	envBaseURL := getEnv("BASE_URL", defaultBaseURL)
 	envFileStoragePath := getEnv("FILE_STORAGE_PATH", defaultFileStoragePath)
+	envDatabaseDSN := getEnv("DATABASE_DSN", defaultDatabaseDSN)
 
 	// Read from command-line flags
 	address := flag.String("a", envAddress, "address to start the HTTP server")
 	baseURL := flag.String("b", envBaseURL, "base URL for shortened URLs")
 	fileStoragePath := flag.String("f", envFileStoragePath, "path to the file for storing URL data")
+	databaseDSN := flag.String("d", envDatabaseDSN, "database DSN")
+
 	flag.Parse()
 
 	return Config{
 		Address:         *address,
 		BaseURL:         *baseURL,
 		FileStoragePath: *fileStoragePath,
+		DatabaseDSN:     *databaseDSN,
 	}
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,25 +1,47 @@
 package router
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/learies/go-url-shortener/config"
 	"github.com/learies/go-url-shortener/internal/handlers"
 	"github.com/learies/go-url-shortener/internal/shortener"
 	"github.com/learies/go-url-shortener/internal/store"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
+
+func connectToDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
 
 func NewRouter(cfg config.Config) http.Handler {
 	store := store.NewURLStore(cfg.FileStoragePath)
 	urlShortener := shortener.NewURLShortener()
 
+	db, err := connectToDB(cfg.DatabaseDSN)
+	if err != nil {
+		log.Fatalf("Error opening database connection: %v", err)
+		os.Exit(1)
+	}
+
 	r := chi.NewRouter()
 	r.Post("/", handlers.PostHandler(store, cfg, urlShortener))
 	r.Post("/api/shorten", handlers.PostAPIHandler(store, cfg, urlShortener))
 	r.Get("/*", handlers.GetHandler(store))
+	r.Get("/ping", handlers.PingHandler(db))
 
 	r.MethodNotAllowed(methodNotAllowedHandler)
+
 	return r
 }
 
